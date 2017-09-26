338bb4eb1deda0f829a8447645b121081279f3fe
saptakbasu7 <31066997+saptakbasu7@users.noreply.github.com>
2017-09-27 00:38:50 +0530

    Update Looping_Images.py

--- Looping_Images.py
+++ Looping_Images.py
@@ -7,16 +7,14 @@ import os
 model = inception_v3.InceptionV3()
 
 path = '../ImageInput/'
-#directory = os.fsencode(directory_in_str)
+
 
 for image_path in os.listdir(path):
     # create the full input path and read the file
     input_path = os.path.join(path, image_path)
 
     if input_path.endswith(".png") or input_path.endswith(".jpg"):
-        #os.path.join(directory, filename))
-        # Load the image file and convert it to a numpy array
-        #print("success")
+   
         img = image.load_img(input_path, target_size=(299, 299))
         input_image = image.img_to_array(img)
 
@@ -32,7 +30,6 @@ for image_path in os.listdir(path):
         predictions = model.predict(input_image)
 
         # Convert the predictions into text and print them
-        #predicted_classes = inception_v3.decode_predictions(predictions, top=1)
         predicted_classes = inception_v3.decode_predictions(predictions, top=1)
         imagenet_id, name, confidence = predicted_classes[0][0]
         print("This is a {} with {:.4}% confidence!".format(name, confidence * 100))
